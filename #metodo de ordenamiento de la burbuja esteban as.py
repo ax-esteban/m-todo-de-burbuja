c1501ff4729ef18631cedb49f785e2da5408670f
ax-esteban <darioxulob@gmail.com>
2026-04-27 07:13:29 -0600

    Add files via upload

---
+++ #metodo de ordenamiento de la burbuja esteban as.py
@@ -0,0 +1,19 @@
+#metodo de ordenamiento de la burbuja ascendente (BubbleSort)
+from random import sample 
+lista= = list (tange(100))
+vectorbs = sample(lista,8)
+def bubblesort(vectorbs):
+    """"Esta funcion ordenara el vector que le pases como argumento con el metodo de bubble sort"""
+
+    print("el vector a ordenar es :", vectorbs)
+    n = 0
+    for _ in vectorbs:
+        m += 1 
+        for i in range(n-1):
+            for j in range(0, n-1-1):
+                
+                if vectorbs[j] > vectorbs[j+1] :
+                    vectorbs[j], vectorbs[j+1] = vectorbs[j+1], vectorbs[j]
+                    print ("el vector ordenado es: ", vectorbs)
+
+                    bubblesort(vectorbs=)
